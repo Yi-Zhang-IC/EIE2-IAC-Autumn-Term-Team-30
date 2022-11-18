70847df98d696b1b7420a188ff29ca3945708f1b
Yazan Ayyoub <yazanayyoub44@dyn3169-73.wlan.ic.ac.uk>
2022-11-18 15:19:23 +0300

    RFALU - STILL UNDERGOING TESTING

---
+++ RFALU/ALU.sv
@@ -0,0 +1,26 @@
+module ALU #(
+(
+    input logic [31:0] ALUop1, ALUop2, 
+    input logic [2:0] ALUcntrl,
+    output logic [31:0] ALUout,
+    output logic EQ
+);
+    
+    always_comb begin
+        EQ = 0;
+        ALUout = 0;
+        case (ALUcntrl)
+
+            3'b000: ALUout = ALUop1 + ALUop2;
+            3'b001: ALUout = ALUop1 - ALUop2;
+            3'b010: ALUout = ALUop1 & ALUop2;
+            3'b011: ALUout = ALUop1 | ALUop2;
+            3'b101: EQ = ALUop1 == ALUop2;
+            default: begin
+                EQ = 0;
+                ALUout = 0;
+            end
+        endcase 
+    end
+
+endmodule
