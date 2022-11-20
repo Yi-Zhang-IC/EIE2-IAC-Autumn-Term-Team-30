5301b3a4118183fc4f9dd6b0c3cabd1537bb1aaa
SsagatroNN <omar.khatib03@outlook.com>
2022-11-20 22:50:17 +0000

    Edited init.sh allowing others to run it on their vbuddy

--- omar/init.sh
+++ omar/init.sh
@@ -1,9 +1,9 @@
-DEVICEPORT=$(cat vbuddy.cfg | head -n 1)
+# DEVICEPORT=$(cat vbuddy.cfg | head -n 1)
 
-stty -F "${DEVICEPORT}" 115200 speed -parenb -parodd -cmspar cs8 -hupcl -cscpub cread clocal -crtscts -ignbrk -brkint -ignpar -parmrk -inpck -istrip -inlcr -igncr -icrnl -ixon -ixoff -iuclc -ixany -imaxbel -iutf8 -opost -olcuc -ocrnl -onlcr -onocr -onlret -ofill -ofdel nl0 cr0 tab0 bs0 vt0 ff0 -isig -icanon iexten -echo echoe echok -echonl -noflsh -xcase -toscpu -echoprt echoctl echoke -flusho -extproc
-echo -ne '\r\n' > "${DEVICEPORT}"
-echo -ne 'execfile("vbuddy.py")' > "${DEVICEPORT}"
-echo -ne '\r\n' > "${DEVICEPORT}"
+# stty -F "${DEVICEPORT}" 115200 speed -parenb -parodd -cmspar cs8 -hupcl -cscpub cread clocal -crtscts -ignbrk -brkint -ignpar -parmrk -inpck -istrip -inlcr -igncr -icrnl -ixon -ixoff -iuclc -ixany -imaxbel -iutf8 -opost -olcuc -ocrnl -onlcr -onocr -onlret -ofill -ofdel nl0 cr0 tab0 bs0 vt0 ff0 -isig -icanon iexten -echo echoe echok -echonl -noflsh -xcase -toscpu -echoprt echoctl echoke -flusho -extproc
+# echo -ne '\r\n' > "${DEVICEPORT}"
+# echo -ne 'execfile("vbuddy.py")' > "${DEVICEPORT}"
+# echo -ne '\r\n' > "${DEVICEPORT}"
 
 # cleanup
 rm -rf obj_dir
